f29f2119d7dd470801801ccc353e83c988b6b17c
bigsir24 <bigsir524@gmail.com>
2024-07-01 22:24:46 +0200

    Added options for scale and number of hearts

--- src/main/java/bigsir/simplehealthindicator/SHealthIndicator.java
+++ src/main/java/bigsir/simplehealthindicator/SHealthIndicator.java
@@ -1,19 +1,28 @@
 package bigsir.simplehealthindicator;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.options.components.OptionsCategory;
+import net.minecraft.client.gui.options.components.ToggleableOptionComponent;
+import net.minecraft.client.gui.options.data.OptionsPages;
+import net.minecraft.client.option.GameSettings;
+import net.minecraft.client.option.RangeOption;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.util.ClientStartEntrypoint;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
-public class SHealthIndicator implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
+public class SHealthIndicator implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint, ClientStartEntrypoint {
     public static final String MOD_ID = "simplehealthindicator";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
     public void onInitialize() {
         LOGGER.info("Simple Health Indicator initialized.");
     }
+	public static RangeOption maxHearts;
+	public static RangeOption heartScale;
 
 	@Override
 	public void beforeGameStart() {
@@ -33,5 +42,23 @@ public class SHealthIndicator implements ModInitializer, GameStartEntrypoint, Re
 	@Override
 	public void initNamespaces() {
 
+	}
+
+	@Override
+	public void beforeClientStart() {
+
+	}
+
+	@Override
+	public void afterClientStart() {
+		GameSettings settings = Minecraft.getMinecraft(Minecraft.class).gameSettings;
+		maxHearts = new RangeOption(settings, "simplehealthindicator.maxhearts", 3, 9);
+		heartScale = new RangeOption(settings, "simplehealthindicator.heartscale", 50 ,150);
+
+		OptionsPages.VIDEO.withComponent(
+			new OptionsCategory("simplehealthindicator.category")
+				.withComponent(new ToggleableOptionComponent<>(maxHearts))
+				.withComponent(new ToggleableOptionComponent<>(heartScale))
+		);
 	}
 }
