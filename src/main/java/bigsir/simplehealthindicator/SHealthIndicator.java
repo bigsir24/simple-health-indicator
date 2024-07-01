8dd49b65f57deb896e9b862fc973191789f28020
bigsir24 <bigsir524@gmail.com>
2024-07-01 22:34:30 +0200

    Added mod category to options

--- src/main/java/bigsir/simplehealthindicator/SHealthIndicator.java
+++ src/main/java/bigsir/simplehealthindicator/SHealthIndicator.java
@@ -4,9 +4,11 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.options.components.OptionsCategory;
 import net.minecraft.client.gui.options.components.ToggleableOptionComponent;
+import net.minecraft.client.gui.options.data.OptionsPage;
 import net.minecraft.client.gui.options.data.OptionsPages;
 import net.minecraft.client.option.GameSettings;
 import net.minecraft.client.option.RangeOption;
+import net.minecraft.core.item.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import turniplabs.halplibe.util.ClientStartEntrypoint;
@@ -21,6 +23,7 @@ public class SHealthIndicator implements ModInitializer, GameStartEntrypoint, Re
     public void onInitialize() {
         LOGGER.info("Simple Health Indicator initialized.");
     }
+	public static OptionsPage optionsPage;
 	public static RangeOption maxHearts;
 	public static RangeOption heartScale;
 
@@ -54,8 +57,10 @@ public class SHealthIndicator implements ModInitializer, GameStartEntrypoint, Re
 		GameSettings settings = Minecraft.getMinecraft(Minecraft.class).gameSettings;
 		maxHearts = new RangeOption(settings, "simplehealthindicator.maxhearts", 3, 9);
 		heartScale = new RangeOption(settings, "simplehealthindicator.heartscale", 50 ,150);
+		optionsPage = new OptionsPage("simplehealthindicator.options", Item.foodApple.getDefaultStack());
+		OptionsPages.register(optionsPage);
 
-		OptionsPages.VIDEO.withComponent(
+		optionsPage.withComponent(
 			new OptionsCategory("simplehealthindicator.category")
 				.withComponent(new ToggleableOptionComponent<>(maxHearts))
 				.withComponent(new ToggleableOptionComponent<>(heartScale))
