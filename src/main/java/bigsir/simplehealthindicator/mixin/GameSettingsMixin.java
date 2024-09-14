49107865e6d13b7959639b884abe331cec34e666
bigsir24 <bigsir524@gmail.com>
2024-09-15 00:52:41 +0200

    Added options to GameSettings (saving fix)

--- src/main/java/bigsir/simplehealthindicator/mixin/GameSettingsMixin.java
+++ src/main/java/bigsir/simplehealthindicator/mixin/GameSettingsMixin.java
@@ -1,16 +1,26 @@
 package bigsir.simplehealthindicator.mixin;
 
 import bigsir.simplehealthindicator.SHealthIndicator;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.option.GameSettings;
 import net.minecraft.client.option.Option;
 import net.minecraft.client.option.RangeOption;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.io.File;
 
 @Mixin(value = GameSettings.class, remap = false)
 public abstract class GameSettingsMixin {
+
+	@Unique
+	public RangeOption maxHearts;
+	@Unique
+	public RangeOption heartScale;
 
 	@Inject(method = "getDisplayString", at = @At("TAIL"), cancellable = true)
 	public void offsetDisplayValue(Option<?> option, CallbackInfoReturnable<String> cir){
@@ -19,5 +29,13 @@ public abstract class GameSettingsMixin {
 		}else if(option == SHealthIndicator.heartScale){
 			cir.setReturnValue( (((RangeOption)option).getValueIndex() + 50) / 100.0 + "x");
 		}
+	}
+
+	@Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lcom/b100/utils/ReflectUtils;getAllObjects(Ljava/lang/Class;Ljava/lang/Class;Ljava/lang/Object;)[Ljava/lang/Object;"))
+	public void addOptions(Minecraft minecraft, File file, CallbackInfo ci){
+		SHealthIndicator.optionsInit((GameSettings) (Object)this);
+
+		this.maxHearts = SHealthIndicator.maxHearts;
+		this.heartScale = SHealthIndicator.heartScale;
 	}
 }
